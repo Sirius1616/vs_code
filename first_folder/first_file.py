b72aae4ce0e190018080fa8f35e4d7dff667c52b
John Ezekiel <siriusa1.615@gmail.com>
2023-11-17 20:08:52 +0100

    new_value

---
+++ first_folder/first_file.py
@@ -0,0 +1,13 @@
+class Parent:
+    def __init__(self, colour, size):
+        self.color = colour
+        self.size = size
+
+    def plant(self):
+        print("The colour we don't like will surface the other day")
+        return (7)
+    
+
+apple = Parent("Green", 23)
+
+apple.plant()
